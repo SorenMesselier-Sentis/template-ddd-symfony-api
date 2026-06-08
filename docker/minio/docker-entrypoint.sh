fbe0c789543d5d6de30491e3834dbe63693eaf60
SorenMesselier-Sentis <soren.messelier@gmail.com>
2026-06-08 11:19:33 +0200

    fix(minIO): Fix minIO Docker setup

--- docker/minio/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-if command -v mc >/dev/null 2>&1; then
-    mc alias set local "http://127.0.0.1:9000" "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}" >/dev/null
-fi
-
-exec /usr/bin/minio "$@"
